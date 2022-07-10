597e6d9f842b038256ee0004d92742c497712637
Enes Döner <BusinessEnesDoner@gmail.com>
2022-07-10 23:28:30 +0300

    apiden veri getirme özelligi eklendi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+
 
 @Component({
   selector: 'app-product',
@@ -8,9 +11,17 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
   products:Product[] = [];
-  constructor() { }
+  apiUrl="https://localhost:44314/api/products/getall"
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts(){
+    this.httpClient
+    .get<ProductResponseModel>(this.apiUrl)
+    .subscribe((response)=>
+    {this.products=response.data});
+  }
 }
