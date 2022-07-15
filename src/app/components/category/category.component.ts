afb1cc664a874e32b1635119632cabfd265e1c0f
Enes Döner <BusinessEnesDoner@gmail.com>
2022-07-15 17:50:00 +0300

    pipe eklendi

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -9,6 +9,7 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class CategoryComponent implements OnInit {
   categories:Category[]=[];
+  currentCategory:Category;
   constructor(private categoryservice:CategoryService ) { }
 
   ngOnInit(): void {
@@ -17,9 +18,25 @@ export class CategoryComponent implements OnInit {
   getCategories(){
     this.categoryservice.getCategories().subscribe(response=>{
       this.categories=response.data;
-
     })
-
   }
-
+  setCurrentCategory(category:Category){
+    this.currentCategory=category;
+  }
+  getCurrentCategoryClass(category:Category){
+    if(category==this.currentCategory){
+      return "list-group-item active"
+    }
+    else
+    {return "list-group-item"}
+  }
+  getAllCategoryClass(){
+    if(!this.currentCategory){
+      return "list-group-item active"
+    }
+    else
+    {return "list-group-item"}
+  }
 }
+
+
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,13 +8,19 @@ import { ListResponseModel } from '../models/listResponseModel';
 })
 export class ProductService {
   
-  apiUrl="https://localhost:44314/api/products/getall"
+  apiUrl="https://localhost:44314/api/"
 
   constructor(private httpClient:HttpClient) { }
   
   getProducts():Observable<ListResponseModel<Product>>{
+    let newPath =this.apiUrl + "products/getall";
     return this.httpClient
-      .get<ListResponseModel<Product>>(this.apiUrl);
+      .get<ListResponseModel<Product>>(newPath);
+  }
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
+    let newPath =this.apiUrl + "products/getbycategory?id="+ categoryId;
+    return this.httpClient
+      .get<ListResponseModel<Product>>(newPath);
   }
 
 }
